e888dcd1e5604f18775c1d819a961edc47f75f3e
Smartisek <domalipdominik@gmail.com>
2024-03-11 11:47:27 +0000

    find all cars sql

--- src/main/java/BusinessObjects/App.java
+++ src/main/java/BusinessObjects/App.java
@@ -1,16 +1,36 @@
 package BusinessObjects;
 
+import DAOs.CarDaoInterface;
+import DAOs.MySqlCarDao;
 import DAOs.MySqlDao;
+import DTOs.CarClass;
 import Exception.DaoException;
 
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) throws DaoException {
+    public static void main(String[] args){
+        CarDaoInterface IUserDao = new MySqlCarDao();
 
-        MySqlDao dd = new MySqlDao();
-        dd.getConnection();
+        try {
+            System.out.println("\nCall findAllCars()");
+            List<CarClass> cars = IUserDao.findAllCars();
 
+            if(cars.isEmpty()){
+                System.out.println("No cars in the system");
+            } else {
+                for(CarClass car : cars){
+                    System.out.println(car.toString());
+                }
+            }
+        } catch (DaoException e){
+            e.printStackTrace();
+        }
     }
 }
+
+
+
 //    public void start() {
 //
 //        System.out.println("\nSample 1 - Connecting to MySQL Database called \"test\" using MySQL JDBC Driver");
