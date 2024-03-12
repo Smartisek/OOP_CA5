6a3d3fc53df0ee8e8a642b19d114512bb7da5701
Smartisek <domalipdominik@gmail.com>
2024-03-12 17:57:06 +0000

    finishing structure of feature 2

--- src/main/java/DAOs/MySqlCarDao.java
+++ src/main/java/DAOs/MySqlCarDao.java
@@ -56,12 +56,48 @@ public class MySqlCarDao extends MySqlDao implements CarDaoInterface{
     //    Feature 2 – Find and Display (a single) Entity by Key
 //    e.g. getPlayerById(id ) – return a single entity (DTO) and display its contents.
 
+//    **** Dominik's code ****
     public CarClass findCarById(int id) throws DaoException{
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         CarClass car = null;
-        
+        try {
+//            Everything in the try block will try to execute code inside, if not
+            connection = this.getConnection();
+            String query = "SELECT * FROM car WHERE id = ?";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                int carId = resultSet.getInt("ID");
+                String model = resultSet.getString("MODEL");
+                String brand = resultSet.getString("BRAND");
+                String colour = resultSet.getString("COLOUR");
+                int production_year = resultSet.getInt("PRODUCTION_YEAR");
+                int price = resultSet.getInt("PRICE");
+
+                car = new CarClass(carId, model, brand, colour, production_year, price);
+            }
+        } catch (SQLException e){
+            throw new DaoException("findCarById()" + e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null){
+                    resultSet.close();
+                }
+                if(preparedStatement != null){
+                    preparedStatement.close();
+                }
+                if(connection != null){
+                    freeConnection(connection);
+                }
+            } catch (SQLException e){
+                throw new DaoException("findCarById()" + e.getMessage());
+            }
+        }
+        return car;
     }
 
 
