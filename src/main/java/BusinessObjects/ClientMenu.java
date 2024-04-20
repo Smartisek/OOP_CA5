5aa074ee9f032869bda12bfd92bc7269e574d6c8
Smartisek <domalipdominik@gmail.com>
2024-04-20 16:31:08 +0100

    changing client interface to have continuous input until quit

--- src/main/java/BusinessObjects/ClientMenu.java
+++ src/main/java/BusinessObjects/ClientMenu.java
@@ -37,55 +37,64 @@ public class ClientMenu {
                 ){
             System.out.println("Client Menu message: The Client Menu is running and has connected to the server.");
             Scanner consoleInput = new Scanner(System.in);
-            System.out.println("Valid commands: " + "\n" + "display ID" + "\n" + "displayAll" + "\n" + "add model brand colour production_year price" +
-                    "\n" + "delete ID");
-            System.out.println("Enter command: ");
-            String userInput = consoleInput.nextLine();
-            out.println(userInput);
+            System.out.println("Valid commands: " + "\n" + "displayEntity ID" + "\n" + "displayAll" + "\n" + "add model brand colour production_year price" +
+                    "\n" + "delete ID" + "\n" + "quit");
+           String userInput = "";
 
-            if(userInput.startsWith("displayEntity")){
-                // receive output from server in Json format
-                String carJson = in.readLine();
-                // turn json format into car class and then print into console
-                CarClass car = JsonConverter.fromJson(carJson);
-                System.out.println("Client Menu message: Response from server: \"" + car + "\n" );
-            } else if(userInput.startsWith("displayAll")){
-                String cars = in.readLine();
-                System.out.println("Client Menu message: Received from server: " +"\n" + cars + "\n");
-                List<CarClass> allCars = JsonConverter.JsonToCarList(cars);
-                for(CarClass car : allCars){
-                    System.out.println(car);
-                }
-            } else if(userInput.startsWith("add")){
+            while(!userInput.startsWith("quit")){
+                System.out.println("Enter command: ");
+                userInput = consoleInput.nextLine();
+                out.println(userInput);
+
+                if(userInput.startsWith("displayEntity")){
+                    // receive output from server in Json format
+                    String carJson = in.readLine();
+                    // turn json format into car class and then print into console
+                    CarClass car = JsonConverter.fromJson(carJson);
+                    System.out.println("Client Menu message: Response from server: \"" + car + "\n" );
+                } else if(userInput.startsWith("displayAll")){
+                    String cars = in.readLine();
+                    System.out.println("Client Menu message: Received from server: " +"\n" + cars + "\n");
+                    List<CarClass> allCars = JsonConverter.JsonToCarList(cars);
+                    for(CarClass car : allCars){
+                        System.out.println(car);
+                    }
+                } else if(userInput.startsWith("add")){
 //                split input into parts so we can find parts for input
-                String[] parts = userInput.split(" ");
+                    String[] parts = userInput.split(" ");
 //                parsing string data into integer needed
-                int production_year = Integer.parseInt(parts[4]);
-                int price = Integer.parseInt(parts[5]);
+                    int production_year = Integer.parseInt(parts[4]);
+                    int price = Integer.parseInt(parts[5]);
 //                creating an instance for the requested insert
-                CarClass insertRequest = new CarClass(0, parts[1], parts[2], parts[3], production_year, price);
-                String jsonRequest = JsonConverter.carObjectToJson(insertRequest);
-                System.out.println("Client Menu message: Sending out " + jsonRequest);
+                    CarClass insertRequest = new CarClass(0, parts[1], parts[2], parts[3], production_year, price);
+                    String jsonRequest = JsonConverter.carObjectToJson(insertRequest);
+                    System.out.println("Client Menu message: Sending out " + jsonRequest);
 //                send the request to the server
-                out.println(jsonRequest);
-                System.out.println("Client Menu message: Receiving response from server...");
+                    out.println(jsonRequest);
+                    System.out.println("Client Menu message: Receiving response from server...");
 //                get response from the server with entity or error message
-                String newCar = in.readLine();
-                System.out.println("Client Menu message: New entity was added into database. New entity: " + "\n" + newCar);
-            } else if(userInput.startsWith("delete")){
+                    String newCar = in.readLine();
+                    System.out.println("Client Menu message: New entity was added into database. New entity: " + "\n" + newCar);
+                } else if(userInput.startsWith("delete")){
 //                splitting input to be able to pass id in
-                String[] parts = userInput.split(" ");
-                int id = Integer.parseInt(parts[1]);
+                    String[] parts = userInput.split(" ");
+                    int id = Integer.parseInt(parts[1]);
 //                using dao function find car by id so we can put it into json format, this could be just done with request as int id but since the specification
 //                says to send to server request in Json, I am using this function to get car with that id and parsing it into json
-                CarClass request = IUserDao.findCarById(id);
+                    CarClass request = IUserDao.findCarById(id);
 //                carclass to json
-                String requestJson = JsonConverter.carObjectToJson(request);
-                System.out.println("Client Menu message: Sending request to server to delete: " + requestJson);
+                    String requestJson = JsonConverter.carObjectToJson(request);
+                    System.out.println("Client Menu message: Sending request to server to delete: " + requestJson);
 //                send request to sever
-                out.println(requestJson);
-                String income = in.readLine();
-                System.out.println(income + requestJson);
+                    out.println(requestJson);
+                    String income = in.readLine();
+                    System.out.println(income + requestJson);
+                }else if(userInput.startsWith("quit")){
+                    out.println("Client menu is being terminated.");
+                    System.out.println("Client Menu message: This client is being terminated.");
+//                    after break statement the while loop exits and that ends the clients session, after try statement ensures that socket, printWritter and buffer are closed and disconnects from server
+                    break;
+                }
             }
         } catch (IOException e){
             System.out.println("Client message: IOException: " + e);
--- src/main/java/BusinessObjects/Server.java
+++ src/main/java/BusinessObjects/Server.java
@@ -25,6 +25,7 @@ public class Server {
         CarDaoInterface IUserDao = new MySqlCarDao();
         JsonConverter JsonConverter = new JsonConverter();
         try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT_NUMBER); ){
+
             while (true){
                 // create a connection, server accepts connection from client
                 try (Socket clientSocket = serverSocket.accept();
@@ -36,61 +37,67 @@ public class Server {
 //                    Message for when we successfully connect to client
                     System.out.println("Server Message: A Client Menu has connected.");
 //                    This makes the program wait until we read a request (input) from client menu
-                    String request = in.readLine();
+                    String request;
 
-                    System.out.println("Server Message: Received from Client Menu: \"" + request + "\n");
+                    while((request = in.readLine()) != null){
+                        System.out.println("Server Message: Received from Client Menu: \"" + request + "\n");
 //  Logic for handling the request from client
-                    if(request.startsWith("displayEntity")){
+                        if(request.startsWith("displayEntity")){
 //                        put request into an array and split it by space
-                        String[] parts = request.split(" ");
+                            String[] parts = request.split(" ");
 //                        second part of request will be id that gets passed into IUserDao function
-                        int id = Integer.parseInt(parts[1]);
-                        CarClass car = IUserDao.findCarById(id);
+                            int id = Integer.parseInt(parts[1]);
+                            CarClass car = IUserDao.findCarById(id);
 //                        convert output class into JSon
-                        String carJson = JsonConverter.carObjectToJson(car);
-                        out.println(carJson);
-                    } else if(request.startsWith("displayAll")){
-                        List<CarClass>allCars =IUserDao.findAllCars();
-                        String carsJson = JsonConverter.carListToJson(allCars); //*********
-                        System.out.println("Server Message: Sending to Client Menu: " + "\n" + carsJson + "\n");
-                        out.println(carsJson);
-                    } else if(request.startsWith("add")){
+                            String carJson = JsonConverter.carObjectToJson(car);
+                            out.println(carJson);
+                        } else if(request.startsWith("displayAll")){
+                            List<CarClass>allCars =IUserDao.findAllCars();
+                            String carsJson = JsonConverter.carListToJson(allCars); //*********
+                            System.out.println("Server Message: Sending to Client Menu: " + "\n" + carsJson + "\n");
+                            out.println(carsJson);
+                        } else if(request.startsWith("add")){
 //                        get the request from client in json format
-                        String jsonRequest = in.readLine();
-                        System.out.println("Server message: Server receives " + jsonRequest);
+                            String jsonRequest = in.readLine();
+                            System.out.println("Server message: Server receives " + jsonRequest);
 //                        convert json request into car class object
-                        CarClass carRequest = JsonConverter.fromJson(jsonRequest);
+                            CarClass carRequest = JsonConverter.fromJson(jsonRequest);
 //                        calling onto insertCar function in DAO with our interface and passing in requested data from carRequest instance
-                        CarClass newCar = IUserDao.insertCar(carRequest.getModel(), carRequest.getBrand(), carRequest.getColour(), carRequest.getProduction_year(), carRequest.getPrice());
+                            CarClass newCar = IUserDao.insertCar(carRequest.getModel(), carRequest.getBrand(), carRequest.getColour(), carRequest.getProduction_year(), carRequest.getPrice());
 //                        if newCar is not null meaning it was added so print message and send result to client
-                        if(newCar != null){
-                            System.out.println("Server message: Entity successfully added. Entity: " + "\n" + newCar);
-                            String jsonNewCar = JsonConverter.carObjectToJson(newCar);
-                            out.println(jsonNewCar);
+                            if(newCar != null){
+                                System.out.println("Server message: Entity successfully added. Entity: " + "\n" + newCar);
+                                String jsonNewCar = JsonConverter.carObjectToJson(newCar);
+                                out.println(jsonNewCar);
 //                            otherwise print message when failed to add, meaning entity already exists or was not able to insert
-                        } else {
-                            System.out.println("Server message: Entity failed to add.");
-                        }
-                    } else if(request.startsWith("delete")){
+                            } else {
+                                System.out.println("Server message: Entity failed to add.");
+                            }
+                        } else if(request.startsWith("delete")){
 //                        read request from client
-                        String requestDelete = in.readLine();
-                        System.out.println("Server message: Server receives request to delete: " + requestDelete);
+                            String requestDelete = in.readLine();
+                            System.out.println("Server message: Server receives request to delete: " + requestDelete);
 //                        put request into car class from json
-                        CarClass requestCar = JsonConverter.fromJson(requestDelete);
+                            CarClass requestCar = JsonConverter.fromJson(requestDelete);
 //                        call on delele requested car by id passing id from requestCar
-                        IUserDao.deleteCarById(requestCar.getId());
+                            IUserDao.deleteCarById(requestCar.getId());
 //                        delete check, after car was deleted, try look for it again, if not found that means delete was successful
-                        CarClass deleteCheck = IUserDao.findCarById(requestCar.getId());
+                            CarClass deleteCheck = IUserDao.findCarById(requestCar.getId());
 //                        if after delete we can not find the entity so it is null, means successful delete and send message to client
-                        if(deleteCheck == null){
-                            System.out.println("Server message: The entity with id: " + requestCar.getId() + " was successfully deleted");
-                            out.println("Delete successful for: ");
-//                        if we are able to find the entity after delete than delete failed and send the message to client 
-                        } else {
-                            System.out.println("Server message: Error entity not deleted");
-                            out.println("Delete failed for: ");
+                            if(deleteCheck == null){
+                                System.out.println("Server message: The entity with id: " + requestCar.getId() + " was successfully deleted");
+                                out.println("Delete successful for: ");
+//                        if we are able to find the entity after delete than delete failed and send the message to client
+                            } else {
+                                System.out.println("Server message: Error entity not deleted");
+                                out.println("Delete failed for: ");
+                            }
+                        } else if(request.startsWith("quit")){
+                            System.out.println("The request from client: " +request + ". Client has been terminated.");
+                            break;
                         }
                     }
+
                 } catch (DaoException e) {
                     throw new RuntimeException(e);
                 }
